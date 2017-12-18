e0481f827606879bdfe914fc6b2115d335585e2f
alen_etfos <alenhalilovi490@gmail.com>
2017-12-18 16:44:25 +0100

    Initial commit

---
+++ Kalkulator_Vrijeme.java
@@ -0,0 +1,20 @@
+package Java_Tasks;
+
+import javax.swing.JOptionPane;
+
+public class Kalkulator_Vrijeme {
+	
+	public static void main(String[] args) {
+	
+		
+		int broj_minuta = Integer.parseInt( JOptionPane.showInputDialog("Unesi broj minuta") );
+		int broj_sati = Integer.parseInt( JOptionPane.showInputDialog("Unesi broj sati") );
+		
+		int broj_sekunda = (broj_minuta * 60) + (broj_sati*3600);
+		
+		System.out.println(broj_sekunda);
+		
+		
+	}
+
+}
